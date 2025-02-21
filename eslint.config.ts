a3706c99bbc69093e072c29e551b9bb986bb4cd9
Eduard Guzarevich <e.guzarevich@powerinsight.io>
2025-02-21 08:51:06 +0300

    feat: Configured eslint and prettier. Add tailwind

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,12 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import oxlint from 'eslint-plugin-oxlint'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVitest from '@vitest/eslint-plugin';
+import oxlint from 'eslint-plugin-oxlint';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import globals from 'globals';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import pluginPrettierVue from 'eslint-plugin-prettier-vue';
+import prettierConfig from './.prettierrc.json';
 
 export default defineConfigWithVueTs(
   {
@@ -19,14 +18,46 @@ export default defineConfigWithVueTs(
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
+  { languageOptions: { globals: globals.browser } },
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   oxlint.configs['flat/recommended'],
   skipFormatting,
-)
+  {
+    plugins: {
+      'prettier-vue': pluginPrettierVue,
+    },
+    rules: {
+      'prettier-vue/prettier': ['error', { ...prettierConfig }],
+    },
+  },
+  {
+    plugins: { '@stylistic/ts': stylisticTs },
+    rules: {
+      'prefer-const': 'error',
+      'no-unused-vars': 'error',
+      'no-console': 'error',
+      'no-nested-ternary': 'error',
+      '@stylistic/ts/member-delimiter-style': 'error',
+      'vue/padding-line-between-tags': [
+        'error',
+        [
+          {
+            blankLine: 'always',
+            prev: '*',
+            next: '*',
+          },
+        ],
+      ],
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  }
+);
