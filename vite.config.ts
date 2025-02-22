f4528a9be8bfb03ef32a7ddbc4278248cd78ee8b
Eduard Guzarevich <e.guzarevich@powerinsight.io>
2025-02-22 16:53:11 +0300

    fix: Fix project building to work as lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tsconfigPaths(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -23,7 +25,11 @@ export default defineConfig({
     cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
+      name: '@djuslun/ds',
       formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['vue'],
     },
   },
 });
